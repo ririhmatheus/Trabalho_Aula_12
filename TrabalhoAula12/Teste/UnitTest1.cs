35614deb88265478c31dfa3e669c957e3f2ecee0
ririhmatheus <ririhmatheus@gmail.com>
2018-06-11 22:04:16 -0300

    Criando

---
+++ TrabalhoAula12/Teste/UnitTest1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrabalhoAula12;
+
+namespace Teste
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IdadePositivo()
+        {
+            Aluno P1 = new Aluno();
+
+            P1.Codigo = 1;
+            P1.Nome = "Felipe";
+            P1.Sobrenome = "Vozgeral Çanca";
+            P1.DataNasc = new DateTime(1990, 04, 01);
+
+            Aluno P2 = new Aluno();
+
+            P2.Codigo = 2;
+            P2.Nome = "Aline";
+            P2.Sobrenome = "Branca";
+            P2.DataNasc = new DateTime(2020, 01, 01);
+
+            Aluno P3 = new Aluno();
+
+            P3.Codigo = 3;
+            P3.Nome = "Alfredo";
+            P3.Sobrenome = "Sella III";
+            P3.DataNasc = new DateTime(1986, 03, 12);
+
+            Assert.Equals()
+        }
+
+        [TestMethod]
+        public void IdadeNegativo()
+        {
+            Aluno P1 = new Aluno();
+
+            P1.Codigo = 1;
+            P1.Nome = "Felipe";
+            P1.Sobrenome = "Vozgeral Çanca";
+            P1.DataNasc = new DateTime(1990, 04, 01);
+
+            Aluno P2 = new Aluno();
+
+            P2.Codigo = 2;
+            P2.Nome = "Alin@";
+            P2.Sobrenome = "Branca";
+            P2.DataNasc = new DateTime(2020, 01, 01);
+
+            Aluno P3 = new Aluno();
+
+            P3.Codigo = 3;
+            P3.Nome = "Alfredo";
+            P3.Sobrenome = "Sella III";
+            P3.DataNasc = new DateTime(1986, 03, 12);
+
+        }
+
+        [TestMethod]
+        public void NomePositivo()
+        {
+            Aluno P1 = new Aluno();
+
+            P1.Codigo = 1;
+            P1.Nome = "Felipe";
+            P1.Sobrenome = "Vozgeral Çanca";
+            P1.DataNasc = new DateTime(1990, 04, 01);
+
+            Aluno P2 = new Aluno();
+
+            P2.Codigo = 2;
+            P2.Nome = "Alin@";
+            P2.Sobrenome = "Branca";
+            P2.DataNasc = new DateTime(2020, 01, 01);
+
+            Aluno P3 = new Aluno();
+
+            P3.Codigo = 3;
+            P3.Nome = "Alfredo";
+            P3.Sobrenome = "Sella III";
+            P3.DataNasc = new DateTime(1986, 03, 12);
+
+        }
+
+        [TestMethod]
+        public void NomeNegativo()
+        {
+            Aluno P1 = new Aluno();
+
+            P1.Codigo = 1;
+            P1.Nome = "Felipe";
+            P1.Sobrenome = "Vozgeral Çanca";
+            P1.DataNasc = new DateTime(1990, 04, 01);
+
+            Aluno P2 = new Aluno();
+
+            P2.Codigo = 2;
+            P2.Nome = "Alin@";
+            P2.Sobrenome = "Branca";
+            P2.DataNasc = new DateTime(2020, 01, 01);
+
+            Aluno P3 = new Aluno();
+
+            P3.Codigo = 3;
+            P3.Nome = "Alfredo";
+            P3.Sobrenome = "Sella III";
+            P3.DataNasc = new DateTime(1986, 03, 12);
+
+        }
+
+        [TestMethod]
+        public void ValidaNomePositivo()
+        {
+            Aluno P1 = new Aluno();
+
+            P1.Codigo = 1;
+            P1.Nome = "Felipe";
+            P1.Sobrenome = "Vozgeral Çanca";
+            P1.DataNasc = new DateTime(1990, 04, 01);
+
+            Aluno P2 = new Aluno();
+
+            P2.Codigo = 2;
+            P2.Nome = "Alin@";
+            P2.Sobrenome = "Branca";
+            P2.DataNasc = new DateTime(2020, 01, 01);
+
+            Aluno P3 = new Aluno();
+
+            P3.Codigo = 3;
+            P3.Nome = "Alfredo";
+            P3.Sobrenome = "Sella III";
+            P3.DataNasc = new DateTime(1986, 03, 12);
+
+        }
+
+        [TestMethod]
+        public void ValidaNomeNegativo()
+        {
+            Aluno P1 = new Aluno();
+
+            P1.Codigo = 1;
+            P1.Nome = "Felipe";
+            P1.Sobrenome = "Vozgeral Çanca";
+            P1.DataNasc = new DateTime(1990, 04, 01);
+
+            Aluno P2 = new Aluno();
+
+            P2.Codigo = 2;
+            P2.Nome = "Alin@";
+            P2.Sobrenome = "Branca";
+            P2.DataNasc = new DateTime(2020, 01, 01);
+
+            Aluno P3 = new Aluno();
+
+            P3.Codigo = 3;
+            P3.Nome = "Alfredo";
+            P3.Sobrenome = "Sella III";
+            P3.DataNasc = new DateTime(1986, 03, 12);
+
+        }
+
+        [TestMethod]
+        public void ErroValidacaoPositivo()
+        {
+            Aluno P1 = new Aluno();
+
+            P1.Codigo = 1;
+            P1.Nome = "Felipe";
+            P1.Sobrenome = "Vozgeral Çanca";
+            P1.DataNasc = new DateTime(1990, 04, 01);
+
+            Aluno P2 = new Aluno();
+
+            P2.Codigo = 2;
+            P2.Nome = "Alin@";
+            P2.Sobrenome = "Branca";
+            P2.DataNasc = new DateTime(2020, 01, 01);
+
+            Aluno P3 = new Aluno();
+
+            P3.Codigo = 3;
+            P3.Nome = "Alfredo";
+            P3.Sobrenome = "Sella III";
+            P3.DataNasc = new DateTime(1986, 03, 12);
+
+        }
+
+        [TestMethod]
+        public void ErroValidacaoNegativo()
+        {
+            Aluno P1 = new Aluno();
+
+            P1.Codigo = 1;
+            P1.Nome = "Felipe";
+            P1.Sobrenome = "Vozgeral Çanca";
+            P1.DataNasc = new DateTime(1990, 04, 01);
+
+            Aluno P2 = new Aluno();
+
+            P2.Codigo = 2;
+            P2.Nome = "Alin@";
+            P2.Sobrenome = "Branca";
+            P2.DataNasc = new DateTime(2020, 01, 01);
+
+            Aluno P3 = new Aluno();
+
+            P3.Codigo = 3;
+            P3.Nome = "Alfredo";
+            P3.Sobrenome = "Sella III";
+            P3.DataNasc = new DateTime(1986, 03, 12);
+
+        }
+
+    }
+}
---
+++ TrabalhoAula12/TrabalhoAula12/Aluno.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoAula12
+{
+    public class Aluno
+    {
+        public int Codigo { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public DateTime DataNasc { get; set; }
+
+
+        public int idade()
+        {
+            int idade = DateTime.Today.Year - this.DataNasc.Year;
+
+            return DataNasc;
+        }
+           
+        public string nomeCompleto()
+        {// Deverá retornar o nome concatenado com o sobrenome
+            return "oi";
+        }
+
+        public int validaNome()
+        {//Deverá validar o nome do Aluno com caracteres divergentes
+            return 1;
+        }
+
+        public string erroValidacao(int i)
+        {
+            
+        }
+    }
+}
